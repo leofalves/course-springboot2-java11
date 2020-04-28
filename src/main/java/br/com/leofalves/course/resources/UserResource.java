3a1979c2b43fa2bf9ceff4204bf3b90e32f3f617
Leo Fabiano Alves <leo.fabiano@gmail.com>
2020-04-28 17:56:43 -0300

    Service Layer, Component Registration

--- src/main/java/br/com/leofalves/course/resources/UserResource.java
+++ src/main/java/br/com/leofalves/course/resources/UserResource.java
@@ -1,20 +1,33 @@
 package br.com.leofalves.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.leofalves.course.entities.User;
+import br.com.leofalves.course.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Leo", "leo@gmail.com", "8889-0990", "123456");
-		return ResponseEntity.ok(u);
-		
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok(obj);
 	}
 }
